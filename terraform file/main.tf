895df9f40f92d1ca03647d6fc810404b430aba11
Pradeep Kumar <168929332+PradeepKumar8765@users.noreply.github.com>
2024-09-04 22:45:05 +0530

    Create main.tf

---
+++ terraform file/main.tf
@@ -0,0 +1,24 @@
+resource "aws_instance" "test_server" {
+  ami = "ami-0e86e20dae9224db8"
+  instance_type = "t2.micro"
+  key_name= "k8s"
+  vpc_security_group_ids=["sg-0b2f60d0cb7032a9a"]
+  connection {
+  type = "ssh"
+  user = "ubunut"
+  private_key = file("./k8s.pem")
+  host = self.public_ip
+  }
+provisioner "remote-exec" {
+  inline = ["echo 'wait to start instance' "]
+}
+tags = {
+  Name= "test-server"
+  }
+provisioner "local-exec" {
+  command = "echo ${aws_instance.test-server.public_ip} > inventory"
+  }
+provisioner "local-exec" {
+  command = "ansible-playbook 
+  }
+}
