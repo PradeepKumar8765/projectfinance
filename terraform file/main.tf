4db337b4594683311ada090dae3173a441f083b9
Pradeep Kumar <168929332+PradeepKumar8765@users.noreply.github.com>
2024-09-05 16:22:47 +0530

    Update main.tf

--- terraform file/main.tf
+++ terraform file/main.tf
@@ -1,24 +1,31 @@
+
 resource "aws_instance" "test-server" {
-  ami = "ami-0e86e20dae9224db8"
-  instance_type = "t2.micro"
-  key_name = "k8s"
+  ami                 = "ami-0e86e20dae9224db8"
+  instance_type       = "t2.micro"
+  key_name            = "k8s"
   vpc_security_group_ids = ["sg-0b2f60d0cb7032a9a"]
+
+  tags = {
+    Name = "test-server"
+  }
+
+  # Use the connection block only if you need to use remote-exec provisioner
   connection {
-  type = "ssh"
-  user = "ubuntu"
-  private_key = file("./k8s.pem")
-  host = self.public_ip
+    type        = "ssh"
+    user        = "ubuntu"
+    private_key = file("./k8s.pem")
+    host        = self.public_ip
   }
-provisioner "remote-exec" {
-  inline = ["echo 'wait to start instance' "]
-}
-tags = {
-  Name = "test-server"
+
+  provisioner "remote-exec" {
+    inline = ["echo 'wait to start instance'"]
   }
-provisioner "local-exec" {
-  command = "echo ${aws_instance.test-server.public_ip} > inventory"
+
+  provisioner "local-exec" {
+    command = "echo ${self.public_ip} > inventory"
   }
-provisioner "local-exec" {
-  command = "ansible-playbook /var/lib/jenkins/workspace/pipeline project/terraform file/ansible-playbook.yml"
+
+  provisioner "local-exec" {
+    command = "ansible-playbook /var/lib/jenkins/workspace/pipeline_project/terraform/file/ansible-playbook.yml"
   }
 }
